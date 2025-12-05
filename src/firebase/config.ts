21fc91b02be7477a119867fd7474febd5e8c938d
Tim Holm <TAllenHolm@gmail.com>
2025-12-05 08:18:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "fleet-check-633wb",
+  "appId": "1:540298110924:web:5ab90f3ecb0f533ca3df69",
+  "apiKey": "AIzaSyAUV-T8mKonfudqI8dsvQiEeScOjwKRhqE",
+  "authDomain": "fleet-check-633wb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "540298110924"
+};
